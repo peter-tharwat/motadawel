45f0c8cc9a63bf3634aa4029200280a3dba2afe6
peter <ptharwat@gmail.com>
2021-02-25 13:30:27 +0200

    users

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -29,9 +29,13 @@
                     </div>
                 </div>
                 <div class="col-12 col-lg-5 px-0  pt-3">
-                    <div class="plyr__video-embed" id="player">
+                    <video id="player" playsinline controls data-poster="/path/to/poster.jpg" >
+                      <source src="/videos/video.mp4" type="video/mp4" />
+                    </video>
+                    {{-- <div class="plyr__video-embed" id="player">
+
                         <iframe src="https://www.youtube.com/embed/JLT4Oz5XRbc?origin=https://plyr.io&amp;iv_load_policy=3&amp;modestbranding=1&amp;playsinline=1&amp;showinfo=0&amp;rel=0&amp;enablejsapi=1" allowfullscreen allowtransparency allow="autoplay"></iframe>
-                    </div>
+                    </div> --}}
                 </div>
             </div>
         </div>
